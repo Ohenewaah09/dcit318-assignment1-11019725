e0799fbd8ef6883704056b6f4485f1b0e4fea884
Adaiah Ohenewaah Edinam Tsorblewu <ohenewaahedinam804@gmail.com>
2025-08-02 13:44:35 +0000

    ft. Developed the Ticket Price Calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicketPriceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int originalTicketPrice = 10; // Original ticket price
+            int discountPrice = 7; // Discounted ticket price
+
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 12 || age >= 65)
+            {
+                Console.WriteLine("The Ticket Price is GHC:" + discountPrice); 
+            }
+            else
+            {
+                Console.WriteLine("The Ticket Price is GHC:" + originalTicketPrice);
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
